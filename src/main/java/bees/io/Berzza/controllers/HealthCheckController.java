50d1250d4bb708a4b7edd3aa3a20b0c9ae39d928
ilija <radojkovicika@gmail.com>
2025-06-04 13:34:32 +0200

    Health check for render.com

---
+++ src/main/java/bees/io/Berzza/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package bees.io.Berzza.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
